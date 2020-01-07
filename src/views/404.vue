6b4aab0446bb864a673ffac3740f465586f14783
HJP <786719329@qq.com>
2020-01-07 14:58:50 +0800

    end

---
+++ src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+    <img src="../assets/img/404bg.jpg" alt="" class="no_found">
+</template>
+
+<script>
+    export default {
+        name: "404"
+    }
+</script>
+
+<style scoped>
+    .no_found{
+        width: 100%;
+    }
+</style>
